78526f86ce7e2bfdab63049799f9cebf5651bf28
lindadelacombaz <delacombaz.linda@gmail.com>
2023-07-31 23:16:47 +0000

    wrote a single function to plot without financial data

---
+++ R/plot_xctr.R
@@ -0,0 +1,52 @@
+#' Plot xctr data on a company or portfolio level, without financial data
+#'
+#' Generate bar plots showing the distribution of xctr_risk_category percentages for each benchmark.
+#'
+#' @param data A data frame like [without_financial]
+#' @param company_name (Optional) A character string specifying the company name.
+#'   If provided, the function will generate the plot for the specified company only.
+#'   If NULL (default), the plot will be generated for the entire portfolio.
+#'
+#' @return A [ggplot] object.
+#'
+#' @export
+#'
+#' @examples
+#' # Example 1: Generate the plot for the entire portfolio
+#' plot_xctr(data)
+#'
+#' # Example 2: Generate the plot for a specific company
+#' plot_xctr(data, company_name = "peter peasant")
+plot_xctr <- function(data, company_name = NULL) {
+  # TODO: do we want to drop NA's everywhere silently?
+  data <- data |>
+    na.omit()
+
+  crucial <- c(
+    "benchmark",
+    "_risk_category"
+  )
+  data |> check_crucial_names(c(names(select(data, matches(crucial)))))
+
+  risk_category_var <- names(select(data, matches("_risk_category")))
+
+  data <- data |>
+    mutate(risk_category_var = factor(data[[risk_category_var]], levels = c("low", "medium", "high")))
+
+  if (!is.null(company_name)) {
+    data <- data |> filter(company_name == .env$company_name)
+  }
+
+  data <- data |>
+    group_by(risk_category_var, benchmark) |>
+    summarize(count = n()) |>
+    group_by(benchmark) |>
+    mutate(percentage = count / sum(count))
+
+  ggplot(data, aes(x = .data$risk_category_var, y = .data$percentage, fill = .data$risk_category_var)) +
+    geom_bar(stat = "identity") +
+    facet_wrap(~ .data$benchmark, scales = "fixed") +
+    fill_score_colors() +
+    theme_tiltplot() +
+    ylim(0, 1)
+}
--- R/plot_xctr_portfolio.R
+++ R/plot_xctr_portfolio.R
@@ -1,34 +1,35 @@
 #' Plot xctr data on a portfolio level
 #'
-#' @param data A data frame like [xctr_toy_data].
+#' @param data A data frame like [without_financial].
 #'
 #' @return A [ggplot] object.
 #' @export
 #'
 #' @examples
-#' plot_xctr_portfolio(xctr_toy_data)
+#' plot_xctr_portfolio(without_financial)
 plot_xctr_portfolio <- function(data) {
   # TODO: do we want to drop NA's everywhere silently?
   data <- data |>
     na.omit()
 
   crucial <- c(
-    "_share",
+    "benchmark",
     "_risk_category"
   )
   data |> check_crucial_names(c(names(select(data, matches(crucial)))))
 
   risk_category_var <- names(select(data, matches("_risk_category")))
-  share_var <- names(data)[grep("_share", names(data))]
 
   data <- data |>
     mutate(risk_category_var = factor(data[[risk_category_var]], levels = c("low", "medium", "high")))
 
-  xctr_portfolio_grouped <- data |>
+  data <- data |>
     group_by(.data$benchmark, risk_category_var) |>
-    summarise(avg_share_value = mean(.data[[share_var]]))
+    summarize(count = n()) |>
+    group_by(benchmark) |>
+    mutate(percentage = count / sum(count))
 
-  ggplot(xctr_portfolio_grouped, aes(x = .data$risk_category_var, y = .data$avg_share_value, fill = .data$risk_category_var)) +
+  ggplot(data, aes(x = .data$risk_category_var, y = .data$percentage, fill = .data$risk_category_var)) +
     geom_bar(stat = "identity") +
     facet_wrap(~ .data$benchmark, scales = "fixed") +
     fill_score_colors() +
