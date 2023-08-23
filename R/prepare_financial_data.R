247a94daf208ad5ac3324c559174ffe49e9cb066
lindadelacombaz <delacombaz.linda@gmail.com>
2023-08-23 00:33:38 +0000

    draft PR for my branch

--- R/prepare_financial_data.R
+++ R/prepare_financial_data.R
@@ -6,7 +6,7 @@
 #' @return A processed data frame ready for plotting
 prepare_financial_data <- function(data) {
   data <- data |>
-    drop_na(-c(.data$equal_weight_finance(), .data$worst_case_finance(), .data$best_case_finance(), .data$main_activity()))
+    drop_na(-c(.data$equal_weight_finance, .data$worst_case_finance, .data$best_case_finance, .data$main_activity))
 
   risk_var <- names_matching(data, risk_category())
 
