0a7ee89d56f4aa1515d8624b37d47daf27939fe2
lindadelacombaze <delacombaz.linda@gmail.com>
2023-09-22 16:23:40 +0000

    modified financial data to see all the risks categories

--- R/german_map.R
+++ R/german_map.R
@@ -6,9 +6,6 @@ create_german_map <- function(data, selected_benchmark, selected_finance_weight)
                           year = 2016,
                           crs = 3035)
 
-  EU28 <- eu_countries %>%
-    select(geo = code, name)
-
   #Shapefile for Germany
   SHP_1 <- SHP_0 %>%
     filter(CNTR_CODE == "DE") %>%
@@ -16,11 +13,8 @@ create_german_map <- function(data, selected_benchmark, selected_finance_weight)
     arrange(geo) %>%
     st_as_sf()
 
-  SHP_1 %>%
-    ggplot() +
-    geom_sf()
-
   #NUTS3 to zip file
+  #Source from European Commission: https://gisco-services.ec.europa.eu/tercet/flat-files
   nuts_de <- read_csv2(
     here("NUTS_DE.csv"),
     col_types = cols(
@@ -33,37 +27,18 @@ create_german_map <- function(data, selected_benchmark, selected_finance_weight)
   colnames(nuts_de) <- c("geo", "postcode")
 
   #Merge to have zip codes with NUTS file
-  SHP_test <- SHP_1 %>%
+  SHP_1 <- SHP_1 %>%
     inner_join(nuts_de, by = "geo")
 
   #Merge Shapefile with financial data
-  financial_test <- financial[c(1, 7), ]
-
-  financial_geo <- financial_test %>%
-    left_join(SHP_test, by = "postcode") %>%
+  financial_geo <- financial %>%
+    filter(benchmark == "isic_sec") %>%
+    left_join(SHP_1, by = "postcode") %>%
     st_as_sf()
 
-  #Create map based on financial geo
-  financial_geo %>%
-    ggplot(aes(fill = xctr_risk_category)) +
-    geom_sf()
-
+  #Create map based on financial geo : two layers; financial data and map
   ggplot() +
     geom_sf(data = financial_geo, mapping = aes(fill = xctr_risk_category), show.legend = TRUE) +
     geom_sf(data = SHP_1, fill = NA) +
     coord_sf()
-
-  #European map
-  SHP_28 <- SHP_0 %>%
-    select(geo = NUTS_ID, geometry) %>%
-    inner_join(EU28, by = "geo") %>%
-    arrange(geo) %>%
-    st_as_sf()
-
-  SHP_28 %>%
-    ggplot() +
-    geom_sf() +
-    scale_x_continuous(limits = c(-10, 35)) +
-    scale_y_continuous(limits = c(35, 65))
-
 }
