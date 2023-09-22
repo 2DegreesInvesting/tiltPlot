9a4e7287414adfe86201d82cd70816bbe014f4bb
lindadelacombaze <delacombaz.linda@gmail.com>
2023-09-22 14:53:33 +0000

    tried europstat

--- R/german_map.R
+++ R/german_map.R
@@ -1,12 +1,34 @@
 create_german_map <- function(data, selected_benchmark, selected_finance_weight) {
 
-  map_data_ger <- map_data('world')[map_data('world')$region == "Germany",]
-  ## The map (maps + ggplot2 )
-  ggplot() +
-    geom_polygon(data = map_data_ger,
-                 aes(x=long, y=lat, group = group),
-                 color = 'red', fill = 'pink') +
-    coord_map() +
-    ggtitle("A map of Germany") +
-    theme(panel.background =element_rect(fill = 'blue'))
+  #Shapefile for European countries
+  SHP_0 <- eurostat::get_eurostat_geospatial(resolution = 10,
+                          nuts_level = 0,
+                          year = 2016)
+
+  EU28 <- eu_countries %>%
+    select(geo = code, name)
+
+  EU27 <- eu_countries %>%
+    filter(code != 'UK') %>%
+    select(geo = code, name)
+
+  SHP_28 <- SHP_0 %>%
+    select(geo = NUTS_ID, geometry) %>%
+    inner_join(EU28, by = "geo") %>%
+    arrange(geo) %>%
+    st_as_sf()
+
+  SHP_27 <- SHP_0 %>%
+    select(geo = NUTS_ID, geometry) %>%
+    inner_join(EU27, by = "geo") %>%
+    arrange(geo) %>%
+    st_as_sf()
+
+  SHP_27 %>%
+    ggplot() +
+    geom_sf() +
+    scale_x_continuous(limits = c(-10, 35)) +
+    scale_y_continuous(limits = c(35, 65))
+
+
 }
