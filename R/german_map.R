38928c16ac75d444e50106b9ecbdb792fb4988c7
lindadelacombaze <delacombaz.linda@gmail.com>
2023-09-22 15:29:10 +0000

    merged financial data with geometry of map file

--- R/german_map.R
+++ R/german_map.R
@@ -2,13 +2,12 @@ create_german_map <- function(data, selected_benchmark, selected_finance_weight)
 
   #Shapefile for European countries
   SHP_0 <- get_eurostat_geospatial(resolution = 10,
-                          nuts_level = 2,
+                          nuts_level = 3,
                           year = 2016,
                           crs = 3035)
 
   EU28 <- eu_countries %>%
     select(geo = code, name)
-
 
   #Shapefile for Germany
   SHP_1 <- SHP_0 %>%
@@ -19,6 +18,32 @@ create_german_map <- function(data, selected_benchmark, selected_finance_weight)
 
   SHP_1 %>%
     ggplot() +
+    geom_sf()
+
+  #NUTS3 to zip file
+  nuts_de <- read_csv2(
+    here("NUTS_DE.csv"),
+    col_types = cols(
+      NUTS3 = col_character(),  # Specify the data type for the "geo" column
+      CODE = col_integer()  # Specify the data type for the "postcode" column
+    ),
+    quote = "'"
+  )
+
+  colnames(nuts_de) <- c("geo", "postcode")
+
+  #Merge to have zip codes with NUTS file
+  SHP_test <- SHP_1 %>%
+    inner_join(nuts_de, by = "geo")
+
+  #Merge Shapefile with financial data
+  financial_geo <- financial %>%
+    right_join(SHP_test, by = "postcode") %>%
+    st_as_sf()
+
+  #Create map based on financial geo
+  financial_geo %>%
+    ggplot(aes(fill = xctr_risk_category)) +
     geom_sf()
 
   #European map
@@ -33,6 +58,9 @@ create_german_map <- function(data, selected_benchmark, selected_finance_weight)
     geom_sf() +
     scale_x_continuous(limits = c(-10, 35)) +
     scale_y_continuous(limits = c(35, 65))
+
+
+
 
 
 }
