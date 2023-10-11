41b9b5530a4565c25c233b4e07b6587046d1bb0d
lindadelacombaze <delacombaz.linda@gmail.com>
2023-10-11 17:23:43 +0000

    created gradient color for each combination of low, medium and high risk categories

--- R/german_map.R
+++ R/german_map.R
@@ -12,32 +12,56 @@ german_map <- function(data, benchmark = c("all", "unit", "tilt_sec", "unit_tilt
   data |> check_crucial_names(names_matching(data, crucial))
   risk_var <- names_matching(data, "_risk_category")
 
-  #Shapefile for European countries
+  # get shapefile of European countries
   shp_0 <- get_eurostat_geospatial(resolution = 10,
                           nuts_level = 3,
                           year = 2016,
                           crs = 3035)
 
-  #Filter for Germany
+  # filter for germany
   shp_1 <- shp_0 |>
     filter(.data$CNTR_CODE == "DE") |>
     select(geo = .data$NUTS_ID, .data$geometry) |>
     arrange(.data$geo) |>
     st_as_sf()
 
-  #Merge to have zip codes with NUTS file
+  # merge to have zip codes with NUTS file
   shp_1 <- shp_1 |>
     inner_join(nuts_de, by = "geo")
 
-  #Merge shapefile with financial data
+  # merge shapefile with financial data
   financial_geo <- financial |>
-    filter(benchmark == "isic_sec") |>
+    filter(benchmark == "unit_isic_sec") |>
     left_join(shp_1, by = "postcode") |>
     st_as_sf()
 
-  #Create map based on financial geo with two layers; financial data and map
+  aggregated_data <- financial_geo |>
+    group_by(postcode, xctr_risk_category) |>
+    summarize(count = n()) |>
+    group_by(postcode) |>
+    mutate(proportion = count / sum(count))
+
+  # apply custom_gradient_color to each row
+  aggregated_data <- aggregated_data |>
+    pivot_wider(names_from = "xctr_risk_category", values_from = "proportion", values_fill = 0) |>
+    mutate(color = pmap(list(high, medium, low), custom_gradient_color))
+
+  # create map based on financial geo with two layers; financial data and map
   ggplot() +
-    geom_sf(data = financial_geo, mapping = aes(fill = xctr_risk_category), show.legend = TRUE) +
+    geom_sf(data = aggregated_data, mapping = aes(fill = color), show.legend = TRUE) +
     geom_sf(data = shp_1, fill = NA) +
     coord_sf()
+}
+
+custom_gradient_color <- function(high, medium, low) {
+  # define RGB values for "high," "medium," and "low"
+  high_color <- c(1, 0, 0)   # Red
+  medium_color <- c(1, 0.5, 0)  # Orange
+  low_color <- c(0, 1, 0)  # Green
+
+  # interpolate the colors based on proportions
+  final_color <- high_color * high + medium_color * medium + low_color * low
+  final_color <- do.call(rgb, as.list(final_color))
+
+  return(final_color)
 }
--- data-raw/nuts_data/nuts_de.R
+++ data-raw/nuts_data/nuts_de.R
@@ -1,7 +1,7 @@
 #NUTS3 to zip file
 #Source from European Commission: https://gisco-services.ec.europa.eu/tercet/flat-files
 nuts_de <- read_csv2(
-  here("data-raw/nuts_de.csv"),
+  here("data-raw/nuts_data/nuts_de.csv"),
   col_types = cols(
     NUTS3 = col_character(),  # Specify the data type for the "geo" column
     CODE = col_integer()  # Specify the data type for the "postcode" column
