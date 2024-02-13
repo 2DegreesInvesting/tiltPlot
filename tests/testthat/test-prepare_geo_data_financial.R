0086b2e1ed25e5f82d82ef62dac4880575c8331f
lindadelacombaze <delacombaz.linda@gmail.com>
2024-02-13 16:26:50 +0000

    deleted files that does not work yet2

--- tests/testthat/test-prepare_geo_data_financial.R
+++
@@ -1,9 +0,0 @@
-test_that("returns an object of the expected class", {
-  data <- tibble(
-    postcode = c(53773L, 53774L, 53775L),
-    xctr_risk_category = c("high", "medium", "low"),
-    benchmark = rep("all", 3)
-  )
-  prepared_data <- prepare_geo_data_financial(data)
-  expect_type(prepared_data, "list")
-})
