8652fec80e6d9736e8da262dda4d2af4e4b7953f
lindadelacombaze <delacombaz.linda@gmail.com>
2024-04-16 17:13:39 +0000

    fixed function in ave()

--- R/scatter_plot_financial.R
+++ R/scatter_plot_financial.R
@@ -107,12 +107,14 @@ prepare_scatter_plot_financial <- function(data, benchmarks, scenario, year) {
 calculate_rank <- function(data, mode, col) {
   rank <- switch(mode,
     "equal_weight_finance" = {
-      rank <- ave(data[[col]], data[["bank_id"]], FUN = mean(x, na.rm = TRUE))
+      rank <- ave(data[[col]], data[["bank_id"]],
+                  FUN = function(x) mean(x, na.rm = TRUE))
     },
     "worst_case_finance" = ,
     "best_case_finance" = {
       data <- data[data[[mode]] != 0, ]
-      rank <- ave(data[[col]], data[["bank_id"]], FUN = mean(x, na.rm = TRUE))
+      rank <- ave(data[[col]], data[["bank_id"]],
+                  FUN = function(x) mean(x, na.rm = TRUE))
     }
   )
   list(rank,data)
