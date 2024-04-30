d898ee97bbdee32fad948633fd8dfdc9a5aec694
lindadelacombaze <delacombaz.linda@gmail.com>
2024-04-30 12:57:20 +0000

    added test for scatter plot

--- tests/testthat/test-scatter_plot_financial.R
+++ tests/testthat/test-scatter_plot_financial.R
@@ -35,18 +35,38 @@ test_that("calculate_rank handles NAs for any mode", {
   expect_true(all(unlist(lapply(results, function(x) all(x$rank == 1)))))
 })
 
-test_that("calculated rank are always equal to or below 1 for each scores", {
+test_that("calculated ranks are between 0 and 1 for each scores", {
   test_rank <- function(data, mode, column) {
     result <- calculate_rank(data, mode, column)
-    rank <- unlist(result[[1]])
-    expect_true(all(rank <= 1))
+    rank <- result[[1]]
+    expect_true(all(rank >= 0 & rank <= 1))
   }
 
   lapply(modes(), function(mode) {
     test_rank(financial, mode, aka("profile_ranking"))
   })
-
   lapply(modes(), function(mode) {
     test_rank(financial, mode, aka("emission_profile"))
   })
+})
+
+test_that("amount_total is correct for each bank_id", {
+  fin <- financial
+  expected_amount_totals <- financial |>
+    group_by(bank_id) |>
+    summarise(expected_total = sum(amount_total))
+
+  p_tr <- plot_scatter_financial_impl(financial, aka("transition_risk"))
+  p_ep <- plot_scatter_financial_impl(financial, aka("emission_profile"))
+
+  actual_amount_totals_tr <- p_tr$data |>
+    group_by(bank_id) |>
+    summarise(actual_total = sum(amount_total))
+
+  actual_amount_totals_ep <- p_ep$data |>
+    group_by(bank_id) |>
+    summarise(actual_total = sum(amount_total))
+
+  expect_identical(expected_amount_totals$expected_total, actual_amount_totals_tr$actual_total)
+  expect_identical(expected_amount_totals$expected_total, actual_amount_totals_ep$actual_total)
 })
