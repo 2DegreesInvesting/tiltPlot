c728125124a3ccb1dfb49ebecd841661c1a167b8
lindadelacombaze <delacombaz.linda@gmail.com>
2024-04-30 16:10:54 +0000

    skip tests

--- tests/testthat/test-map_region_risk.R
+++ tests/testthat/test-map_region_risk.R
@@ -1,11 +1,11 @@
 test_that("returns an object of the expected class", {
-  try({
-    plot <- map_region_risk(without_financial)
-  })
+  skip_on_ci()
+  plot <- map_region_risk(without_financial)
   expect_s3_class(plot, "ggplot")
 })
 
 test_that("returns correct risk category values colors", {
+  skip_on_ci()
   data <- tibble(
     postcode = c(53773L, 53774L, 53775L),
     emission_profile = c("high", "medium", "low"),
@@ -16,12 +16,10 @@ test_that("returns correct risk category values colors", {
     medium = rgb(1, 0.5, 0),
     low = rgb(0, 1, 0)
   )
-  try({
-    plot <- map_region_risk(data)
-    layers <- ggplot_build(plot)$data
-    colors <- layers[[1]]$fill
+  plot <- map_region_risk(data)
+  layers <- ggplot_build(plot)$data
+  colors <- layers[[1]]$fill
 
-    names(colors) <- names(expected_colors)
-    expect_true(identical(expected_colors, colors))
-  })
+  names(colors) <- names(expected_colors)
+  expect_true(identical(expected_colors, colors))
 })
