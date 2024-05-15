85bd27ccc8d0520f83828e78d9e4badcdbe7c61f
lindadelacombaze <delacombaz.linda@gmail.com>
2024-05-15 21:24:15 +0000

    styled

--- tests/testthat/test-map_region_risk.R
+++ tests/testthat/test-map_region_risk.R
@@ -1,7 +1,9 @@
 test_that("returns an object of the expected class", {
   skip_on_ci()
-  data <- example_without_financial(postcode = 53773L,
-                                    !!aka("emission_profile") := c("high", "medium", "low"))
+  data <- example_without_financial(
+    postcode = 53773L,
+    !!aka("emission_profile") := c("high", "medium", "low")
+  )
   plot <- map_region_risk(data)
   expect_s3_class(plot, "ggplot")
 })
@@ -29,7 +31,8 @@ test_that("plots the correct postcodes", {
   skip_on_ci()
   data <- example_without_financial(
     postcode = c(53773L, 53774L, 53775L),
-    !!aka("emission_profile") := c("high", "medium", "low"))
+    !!aka("emission_profile") := c("high", "medium", "low")
+  )
   plot <- map_region_risk(data)
 
   postcodes <- unique(plot$plot_env$aggregated_data$postcode)
@@ -43,7 +46,8 @@ test_that("plots the correct companies", {
   data <- example_without_financial(
     postcode = c(53773L, 53774L, 53775L),
     company_name = letters[1:3],
-    !!aka("emission_profile") := c("high", "medium", "low"))
+    !!aka("emission_profile") := c("high", "medium", "low")
+  )
   plot <- map_region_risk(data)
 
   company_names <- unique(plot$plot_env$data$company_name)
@@ -56,7 +60,8 @@ test_that("plots the selected benchmark", {
   skip_on_ci()
   data <- example_without_financial(
     postcode = c(53773L, 53774L, 53775L),
-    !!aka("emission_profile") := c("high", "medium", "low"))
+    !!aka("emission_profile") := c("high", "medium", "low")
+  )
   plot <- map_region_risk(data, "DE", "all")
 
   benchmark <- unique(plot$plot_env$benchmark)
@@ -69,12 +74,13 @@ test_that("plots the selected mode", {
   skip_on_ci()
   data <- example_without_financial(
     postcode = c(53773L, 53774L, 53775L),
-    !!aka("emission_profile") := c("high", "medium", "low"))
+    !!aka("emission_profile") := c("high", "medium", "low")
+  )
   plot <- map_region_risk(data, "DE", "all", "equal_weight")
 
   mode <- unique(plot$plot_env$mode)
   plot$plot_env$
-  expected_mode <- "equal_weight"
+    expected_mode <- "equal_weight"
 
   expect_equal(mode, expected_mode)
 })
