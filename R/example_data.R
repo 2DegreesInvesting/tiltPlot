9ff9205dcd5765076b2641f5dd9021c463d74cde
lindadelacombaze <delacombaz.linda@gmail.com>
2024-05-15 20:19:10 +0000

    added test for bar plot emission profile

--- R/example_data.R
+++ R/example_data.R
@@ -22,6 +22,22 @@ default_financial <- function(bank_id = "a",
   )
 }
 
+default_without_financial <- function(company_name = "a",
+                                      emission_profile = "medium",
+                                      benchmark = "all",
+                                      equal_weight = 0.1,
+                                      worst_case = 0.1,
+                                      best_case = 0.1) {
+  tibble(
+    company_name = company_name,
+    emission_profile = emission_profile,
+    benchmark = benchmark,
+    equal_weight = equal_weight,
+    worst_case = worst_case,
+    best_case = best_case
+  )
+}
+
 #' Construct example financial data
 #'
 #' @param ... Passed to [tibble()].
@@ -47,3 +63,20 @@ default_financial <- function(bank_id = "a",
 #' id <- "bank_id"
 #' example_financial(!!id := "c")
 example_financial <- tiltIndicator::example_data_factory(default_financial())
+
+
+#' Construct example without financial data
+#'
+#' @param ... Passed to [tibble()].
+#'
+#' @return A [tibble()]
+#' @keywords internal
+#' @family example datasets
+#' @export
+#'
+#' @examples
+#' example_without_financial()
+#'
+#' #' # Add columns
+#' example_without_financial(new = 1)
+example_without_financial <- tiltIndicator::example_data_factory(default_without_financial())
--- tests/testthat/test-bar_plot_emission_profile.R
+++ tests/testthat/test-bar_plot_emission_profile.R
@@ -1,20 +1,53 @@
 test_that("returns an object of the expected class", {
-  plot <- bar_plot_emission_profile(without_financial, benchmarks())
+  data <- example_without_financial()
+  plot <- bar_plot_emission_profile(data, benchmarks())
   expect_s3_class(plot, "ggplot")
 })
 
 test_that("returns correct risk category values", {
-  plot <- bar_plot_emission_profile(without_financial, benchmarks())
+  data <- example_without_financial(!!aka("emission_profile") := c("low", "medium", "high"))
+  plot <- bar_plot_emission_profile(data, benchmarks())
   risk_categories <- levels(plot$data$risk_category_var)
   expected_risk_categories <- c("low", "medium", "high")
   expect_equal(risk_categories, expected_risk_categories)
 })
 
 test_that("returns correct benchmarks values", {
-  plot <- bar_plot_emission_profile(without_financial, benchmarks())
+  data <- example_without_financial()
+  plot <- bar_plot_emission_profile(data, benchmarks())
   benchmarks <- unique(plot$data$benchmark)
-  expected_benchmarks <- without_financial |>
+  expected_benchmarks <- data |>
     pull(benchmark) |>
     unique()
   expect_true(all(benchmarks %in% expected_benchmarks))
+})
+
+test_that("calculated proportions are less or equal to 1", {
+  data <- example_without_financial()
+  bar_plots <- function(mode) {
+    bar_plot_emission_profile(data, benchmarks())
+  }
+  plots <- lapply(modes(), bar_plots)
+
+  lapply(seq_along(modes()), function(i) {
+    proportions <- plots[[i]]$data$proportion
+    expect_true(all(proportions >= 0 & proportions <= 1))
+  })
+})
+
+test_that("risk categories are the correct ones displayed, on a company level", {
+  data <- example_without_financial(company_name = letters[1:2])
+  comp_name <- data[1, "company_name"]
+  expected_risk_cat <- data |>
+    filter(company_name == comp_name$company_name) |>
+    pull(aka("emission_profile")) |>
+    unique() |>
+    as_risk_category()
+
+  plot <- data |>
+    filter(company_name == comp_name$company_name) |>
+    bar_plot_emission_profile(benchmarks())
+  risk_cat <- unique(plot$data$risk_category_var)
+
+  expect_true(identical(sort(risk_cat), sort(expected_risk_cat)))
 })
