294a842e94f8737f807c400dd4642899e20f9e85
lindadelacombaze <delacombaz.linda@gmail.com>
2024-05-15 21:23:27 +0000

    added map region tests

--- tests/testthat/test-map_region_risk.R
+++ tests/testthat/test-map_region_risk.R
@@ -24,3 +24,57 @@ test_that("returns correct risk category values colors", {
   names(colors) <- names(expected_colors)
   expect_true(identical(expected_colors, colors))
 })
+
+test_that("plots the correct postcodes", {
+  skip_on_ci()
+  data <- example_without_financial(
+    postcode = c(53773L, 53774L, 53775L),
+    !!aka("emission_profile") := c("high", "medium", "low"))
+  plot <- map_region_risk(data)
+
+  postcodes <- unique(plot$plot_env$aggregated_data$postcode)
+  expected_postcodes <- unique(data$postcode)
+
+  expect_equal(sort(postcodes), sort(expected_postcodes))
+})
+
+test_that("plots the correct companies", {
+  skip_on_ci()
+  data <- example_without_financial(
+    postcode = c(53773L, 53774L, 53775L),
+    company_name = letters[1:3],
+    !!aka("emission_profile") := c("high", "medium", "low"))
+  plot <- map_region_risk(data)
+
+  company_names <- unique(plot$plot_env$data$company_name)
+  expected_company_names <- unique(data$company_name)
+
+  expect_equal(sort(company_names), sort(expected_company_names))
+})
+
+test_that("plots the selected benchmark", {
+  skip_on_ci()
+  data <- example_without_financial(
+    postcode = c(53773L, 53774L, 53775L),
+    !!aka("emission_profile") := c("high", "medium", "low"))
+  plot <- map_region_risk(data, "DE", "all")
+
+  benchmark <- unique(plot$plot_env$benchmark)
+  expected_benchmark <- unique(data$benchmark)
+
+  expect_equal(sort(benchmark), sort(expected_benchmark))
+})
+
+test_that("plots the selected mode", {
+  skip_on_ci()
+  data <- example_without_financial(
+    postcode = c(53773L, 53774L, 53775L),
+    !!aka("emission_profile") := c("high", "medium", "low"))
+  plot <- map_region_risk(data, "DE", "all", "equal_weight")
+
+  mode <- unique(plot$plot_env$mode)
+  plot$plot_env$
+  expected_mode <- "equal_weight"
+
+  expect_equal(mode, expected_mode)
+})
